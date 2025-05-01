6f9677b0413dd8776a248e748fee8de48295543e
Saha <prs527@usask.ca>
2025-04-30 22:31:27 -0600

    Added Two Sum Code with O(n) Complexity

--- TwoSum.py
+++ TwoSum.py
@@ -1,1 +1,19 @@
-print("hello")
+# Suppose there is a list [1,4,3,6,7,8,10]
+#Which of the two numbers will add upto 10?
+
+def two_sum(list,target):
+    dict={}
+    for idx, val in enumerate(list):
+        compliment= target-list[idx]
+        if compliment in dict:
+            return idx, dict[compliment]
+        else:
+            dict[val]=idx
+
+list=[1,4,3,6,7,8,10]
+print(two_sum(list,4))
+        
+
+print(dict)
+
+
